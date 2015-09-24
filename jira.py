d76a42b122b6114a70f608ea696ade2643d01a98
Si Beaumont <simon.beaumont@citrix.com>
2015-09-24 16:15:58 +0100

    jira: Include DC inbox and simplify query
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- jira.py
+++ jira.py
@@ -4,46 +4,36 @@ import sys
 import time
 import requests
 
-
-def jql(project, priority=None):
-    params = [
-        "teams=xs-ring3",
-        "resolution=empty",
-        "project=" + project,
-    ]
-    if priority:
-        params += ["priority=" + priority]
-    return "+AND+".join(params)
-
-queries = [
-    ("CA", "Blocker"),
-    ("CA", "Critical"),
-    ("CA", "Major"),
-    ("XOP", None),
-    ("SCTX", None),
-]
+queries = {
+    "dc_inbox": "R3 Dash: DC Inbox",
+    "CA,priority=Blocker": "R3 Dash: CA Blocker",
+    "CA,priority=Critical": "R3 Dash: CA Critical",
+    "CA,priority=Major": "R3 Dash: CA Major",
+    "SCTX": "R3 Dash: SCTX",
+    "XOP": "R3 Dash: XOP",
+    "PAR": "R3 Dash: PAR",
+}
 
 counts = {}
 try:
-    base_uri = "https://issues.citrite.net/rest/api/2/search?jql="
-    for (pro, pri) in queries:
-        search_uri = base_uri + jql(pro, pri)
+    for (name, filter) in queries.iteritems():
+        jql = "filter='%s'" % filter
+        search_uri = "https://issues.citrite.net/rest/api/2/search?fields=key&jql=" + jql
         response = requests.get(search_uri).json()
-        counts[(pro, pri)] = response['total']
+        counts[name] = response['total']
 except requests.exceptions.ConnectionError:
-    sys.stderr.write("error: Connection to Github failed")
+    sys.stderr.write("error: Connection to JIRA failed")
     sys.exit(3)
 except ValueError:
-    sys.stderr.write("error: Response from Github API was not JSON")
+    sys.stderr.write("error: Response from JIRA API was not JSON")
     sys.exit(4)
 
 influx_uri = "http://localhost:8086/write?db=inforad"
 timestamp = int(time.time()) * 10**9
-for ((pro, pri), count) in counts.iteritems():
-    pri_tag = "" if pri is None else ",priority=%s" % pri
-    payload = "%s%s value=%d %d" % (pro, pri_tag, count, timestamp)
-    try:
+try:
+    for (key, count) in counts.iteritems():
+        payload = "%s value=%d %d" % (key, count, timestamp)
         resp = requests.post(influx_uri, data=payload)
-    except requests.exceptions.ConnectionError:
-        sys.stderr.write("error: Connection to local influxdb failed")
-        sys.exit(5)
+except requests.exceptions.ConnectionError:
+    sys.stderr.write("error: Connection to local influxdb failed")
+    sys.exit(5)
