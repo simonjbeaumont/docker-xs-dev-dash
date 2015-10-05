ce443cf05bbe13175cc1c5ed84f022879bb4a182
Si Beaumont <simon.beaumont@citrix.com>
2015-10-05 10:28:56 +0100

    tickets: Add --dry-run option
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- tickets.py
+++ tickets.py
@@ -3,7 +3,8 @@
 import sys
 import time
 import requests
-from jira import JIRA
+import argparse
+from jira import JIRA, JIRAError
 
 queries = {
     "dc_inbox": "R3 Dash: DC Inbox",
@@ -15,29 +16,47 @@ queries = {
     "PAR": "R3 Dash: PAR",
 }
 
-# uses ~/.netrc for credentials
-jira = JIRA({"server": "https://issues.citrite.net"})
 
-counts = {}
-try:
-    for (name, filter) in queries.iteritems():
-        jql = "filter='%s'" % filter
-        response = jira.search_issues(jql, maxResults=1, fields='key',
-                                      json_result=True)
-        counts[name] = response['total']
-except requests.exceptions.ConnectionError:
-    sys.stderr.write("error: Connection to JIRA failed")
-    sys.exit(3)
-except ValueError:
-    sys.stderr.write("error: Response from JIRA API was not JSON")
-    sys.exit(4)
+def retrieve_counts():
+    # uses ~/.netrc for credentials
+    jira = JIRA({"server": "https://issues.citrite.net"})
+    counts = {}
+    try:
+        for (name, jira_filter) in queries.iteritems():
+            jql = "filter='%s'" % jira_filter
+            response = jira.search_issues(jql, maxResults=1, fields='key',
+                                          json_result=True)
+            counts[name] = response['total']
+    except JIRAError as e:
+        sys.stderr.write("error: Could not retrieve_counts from JIRA: %s" % e)
+        exit(3)
+    return counts
 
-influx_uri = "http://localhost:8086/write?db=inforad"
-timestamp = int(time.time()) * 10**9
-try:
-    for (key, count) in counts.iteritems():
-        payload = "%s value=%d %d" % (key, count, timestamp)
-        resp = requests.post(influx_uri, data=payload)
-except requests.exceptions.ConnectionError:
-    sys.stderr.write("error: Connection to local influxdb failed")
-    sys.exit(5)
+
+def update_db(counts):
+    influx_uri = "http://localhost:8086/write?db=inforad"
+    timestamp = int(time.time()) * 10**9
+    try:
+        for (key, count) in counts.iteritems():
+            payload = "%s value=%d %d" % (key, count, timestamp)
+            requests.post(influx_uri, data=payload)
+    except requests.exceptions.ConnectionError:
+        sys.stderr.write("error: Connection to local influxdb failed")
+        exit(5)
+
+
+def parse_args_or_exit(argv=None):
+    parser = argparse.ArgumentParser(
+        description='Get number of open ring3 tickets and add to dashboard DB')
+    parser.add_argument('-n', '--dry-run', action='store_true',
+                        help='Just retrieve and print the counts, then exit')
+    return parser.parse_args(argv)
+
+
+if __name__ == "__main__":
+    args = parse_args_or_exit(sys.argv[1:])
+    ticket_counts = retrieve_counts()
+    if args.dry_run:
+        print "Retrieved the following counts: %s" % ticket_counts
+        exit(0)
+    update_db(ticket_counts)
