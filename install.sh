914435d4a24e021f65803324998053bd3595f00b
Si Beaumont <simon.beaumont@citrix.com>
2015-10-05 09:15:42 +0100

    install: Use sudo in script rather than execute with sudo
    This way the right $USER gets expanded and the correct name ends up in the
    cron.d entry.
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 /opt/influxdb/influx -execute 'CREATE DATABASE inforad' >/dev/null
-sed 's!@CWD@!'$PWD'!;s!@USER@!'$USER'!' crontab-entries > /etc/cron.d/ring3-dash
+sudo sed 's!@CWD@!'$PWD'!;s!@USER@!'$USER'!' crontab-entries > /etc/cron.d/ring3-dash
