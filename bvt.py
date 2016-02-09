bcd3d293ab71f134861dce4833a0b63133df438b
Rob Hoes <rob.hoes@citrix.com>
2016-02-09 16:54:04 +0000

    bvt: Record build status
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- bvt.py
+++ bvt.py
@@ -38,11 +38,11 @@ def is_build_action_ok(branch, action):
     return (last_passed_build_num > last_failed_build_num)
 
 
-def update_db(status):
+def update_db(status, table):
     influx_uri = "http://localhost:8086/write?db=inforad"
     timestamp = int(time.time()) * 10**9
     try:
-        data = "bvt_status value=%d %d" % ((1 if bvt_status else 0), timestamp)
+        data = "%s value=%d %d" % (table, (1 if status else 0), timestamp)
         requests.post(influx_uri, data=data)
     except requests.exceptions.ConnectionError:
         sys.stderr.write("error: Connection to local influxdb failed")
@@ -51,7 +51,7 @@ def update_db(status):
 
 def parse_args_or_exit(argv=None):
     parser = argparse.ArgumentParser(
-        description='Get the status of last BVT run and add to dashboard DB')
+        description='Get the status of last build and BVT run and add to dashboard DB')
     parser.add_argument('-n', '--dry-run', action='store_true',
                         help='Just retrieve and print the status, then exit')
     return parser.parse_args(argv)
@@ -59,8 +59,11 @@ def parse_args_or_exit(argv=None):
 
 if __name__ == "__main__":
     args = parse_args_or_exit(sys.argv[1:])
+    build_status = is_build_action_ok("trunk-ring3", "xe-phase-1-build")
     bvt_status = is_build_action_ok("trunk-ring3", "xe-phase-1-test-ring3")
     if args.dry_run:
+        print "Build status: %s" % ("PASSED" if build_status else "FAILED")
         print "BVT status: %s" % ("PASSED" if bvt_status else "FAILED")
         exit(0)
-    update_db(bvt_status)
+    update_db(build_status, "build_status")
+    update_db(bvt_status, "bvt_status")
