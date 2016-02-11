0f427a300c9e2fba89316c762197ef44c8853614
Si Beaumont <simon.beaumont@citrix.com>
2016-02-11 11:16:37 +0000

    docker: Move grafana files to grafana/ dir
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 /opt/influxdb/influx -execute 'CREATE DATABASE inforad' >/dev/null
 sudo sed 's!@CWD@!'$PWD'!;s!@USER@!'$USER'!' crontab-entries > /etc/cron.d/ring3-dash
-sudo cp --backup=numbered grafana.ini /etc/grafana/grafana.ini
+sudo cp --backup=numbered grafana/grafana.ini /etc/grafana/grafana.ini
 sudo chgrp grafana /etc/grafana/grafana.ini
 sudo mkdir -p /var/lib/grafana/dashboards
-sudo cp --backup=numbered dash.json /var/lib/grafana/dashboards/ring3-dash.json
+sudo cp --backup=numbered grafana/dash.json /var/lib/grafana/dashboards/ring3-dash.json
 sudo chgrp grafana /var/lib/grafana/dashboards/ring3-dash.json
